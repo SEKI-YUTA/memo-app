57160b435adc0017ad1809d06cecc14f0dd95bcb
SEKI-YUTA <zard.since1991@gmail.com>
2021-05-27 10:48:10 +0900

    Style MemoListScreen

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.appBar}>
+        <View style={styles.appBarInner}>
+          <Text style={styles.appBarTitle}>Memo App</Text>
+          <Text style={styles.appBarRight}>ログアウト</Text>
+        </View>
+      </View>
+
+      <View>
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2021/05/27</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonLabel}>+</Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +32,70 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    backgroundColor: "#f0f4f8",
+  },
+  appBar: {
+    width: "100%",
+    height: 104,
+    backgroundColor: "#467fd3",
+    justifyContent: "flex-end",
+  },
+  appBarInner: {
+    // backgroundColor: "gray",
+    alignItems: "center",
+  },
+  appBarRight: {
+    position: "absolute",
+    right: 19,
+    bottom: 14,
+    color: "rgba(255,255,255,.8)",
+  },
+  appBarTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  memoListItem: {
+    backgroundColor: "#fff",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    borderBottomWidth: 1,
+    borderColor: "rgba(0,0,0,.15)",
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: "#848484",
+  },
+  circleButton: {
+    backgroundColor: "#467fd3",
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    right: 40,
+    bottom: 40,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    elevation: 8,
+  },
+  circleButtonLabel: {
+    color: "#fff",
+    fontSize: 40,
+    lineHeight: 40,
   },
 });
