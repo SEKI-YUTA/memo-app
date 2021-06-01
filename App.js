78b9e267269b752ce59c8331569981060c548b3b
SEKI-YUTA <zard.since1991@gmail.com>
2021-06-01 19:11:11 +0900

    Create save memo on firesotre Flow

--- App.js
+++ App.js
@@ -11,14 +11,15 @@ import {
   createStackNavigator,
   CardStyleInterpolators,
 } from "@react-navigation/stack";
+
 import firebase from "firebase";
 import { firebaseConfig } from "./env";
-
-const Stack = createStackNavigator();
-
+require("firebase/firestore");
 if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig);
 }
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
--- src/screens/MemoCreateScreen.js
+++ src/screens/MemoCreateScreen.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet, TextInput } from "react-native";
 import AppBar from "../components/AppBar";
 import CircleButton from "../components/CircleButton";
 import KeyboardSafeView from "../components/KeyboardSafeView";
+import firebase from "firebase";
 
 export default MemoCreateScreen = ({ navigation }) => {
+  const [bodyText, setBodyText] = useState("");
+  const handlePress = () => {
+    const { currentUser } = firebase.auth();
+    const db = firebase.firestore();
+    const ref = db.collection(`users/${currentUser.uid}/memos`);
+
+    ref
+      .add({ bodyText: bodyText, updateAt: new Date() })
+      .then((docRef) => {
+        console.log("Created!", docRef.id);
+        navigation.goBack();
+      })
+      .catch((err) => {
+        console.log("Error");
+      });
+  };
   return (
     <KeyboardSafeView style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput value="" multiline style={styles.input} />
+        <TextInput
+          value={bodyText}
+          multiline
+          style={styles.input}
+          onChangeText={(text) => setBodyText(text)}
+          autoFocus
+        />
       </View>
-      <CircleButton
-        name="check"
-        onPress={() => {
-          navigation.goBack();
-        }}
-      />
+      <CircleButton name="check" onPress={handlePress} />
     </KeyboardSafeView>
   );
 };
